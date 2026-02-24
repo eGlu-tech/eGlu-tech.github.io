5829993777d5574ed9e07376a910dfd27b0ff820
siddhant <siddhantkumarupmanyu@gmail.com>
2026-02-25 03:35:30 +0530

    refactor: centralize social links and site info in src/consts.ts

---
+++ src/consts.ts
@@ -0,0 +1,20 @@
+export const SITE_TITLE = 'eGlu.tech';
+export const SITE_DESCRIPTION = 'Notes on technology, design, and engineering.';
+
+export const SOCIAL_LINKS = [
+    {
+        name: 'X',
+        url: 'https://x.com/eglu_tech',
+        icon: 'x'
+    },
+    {
+        name: 'Instagram',
+        url: 'https://instagram.com/eglu.tech',
+        icon: 'instagram'
+    },
+    {
+        name: 'LinkedIn',
+        url: 'https://linkedin.com/company/eglu-tech',
+        icon: 'linkedin'
+    }
+];
