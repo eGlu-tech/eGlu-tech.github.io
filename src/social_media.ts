e11c6af063a6b29505a38f5cc6fc7cceccb8cd8b
siddhant <siddhantkumarupmanyu@gmail.com>
2026-02-25 03:41:31 +0530

    feat: centralize social media links in src/social_media.ts

---
+++ src/social_media.ts
@@ -0,0 +1,5 @@
+export const socialMedia = {
+    linkedinLink: 'https://linkedin.com/company/eglu-tech',
+    instagramLink: 'https://instagram.com/eglu.tech',
+    xLink: 'https://x.com/eglu_tech'
+};
