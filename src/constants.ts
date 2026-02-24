d8eb949e5479b5fd8b847a0373c27a7e94b6985a
siddhant <siddhantkumarupmanyu@gmail.com>
2026-02-25 03:49:09 +0530

    refactor: consolidate social media links into `constants.ts` and update imports

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+
+export const socialMediaLinks = {
+    linkedin: "https://www.linkedin.com/company/eglu-smart-home/",
+    instagram: "https://www.instagram.com/myeglu/",
+    x: 'https://www.instagram.com/myeglu/'
+};
--- src/social_media.ts
+++
@@ -1,5 +0,0 @@
-export const socialMedia = {
-    linkedinLink: "https://www.linkedin.com/company/eglu-smart-home/",
-    instagramLink: "https://www.instagram.com/myeglu/",
-    xLink: 'https://www.instagram.com/myeglu/'
-};
