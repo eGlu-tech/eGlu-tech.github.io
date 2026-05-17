1387e1723349b67ab09c142ce9b56277a3992ba6
siddhant <siddhantkumarupmanyu@gmail.com>
2026-05-17 15:10:34 +0530

    feat: add 'Optimizing Hex Formatting' blog post and update footer

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,6 @@ export const blogName = 'eGlu.tech';
 export const socialMediaLinks = {
     linkedin: "https://www.linkedin.com/company/eglu-smart-home/",
     instagram: "https://www.instagram.com/myeglu/",
-    x: 'https://www.instagram.com/myeglu/'
+    x: 'https://www.instagram.com/myeglu/',
+    website: 'https://www.myeglu.com'
 };
